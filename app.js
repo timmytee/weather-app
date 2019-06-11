40f8bf571e4252b4a1668c2999e47563b6e6b2d0
Tim Tangonan <trstangonan@gmail.com>
2019-06-12 00:25:14 +0800

    Added Skycons

--- app.js
+++ app.js
@@ -11,19 +11,27 @@ window.addEventListener('load', ()=> {
             lat = position.coords.latitude;
 
             const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}https://api.darksky.net/forecast/5867f523e55f8b2d08b88c4f7ea8a291/${lat},${lat}`;
+            const api = `${proxy}https://api.darksky.net/forecast/5867f523e55f8b2d08b88c4f7ea8a291/${lat},${long}`;
             
             fetch(api)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                const { temperature, summary } = data.currently;
+                const { temperature, summary, icon } = data.currently;
                 //Set DOM elements from the API
                 temperatureDegree.textContent = Math.round((temperature -32)/1.8);
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                setIcons(icon, document.querySelector('.icon'));
             });
-        });       
+        });
+    }
+
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
